fa9c7c1c66b9fb901ec50e8d817e97ac9097ec97
amavr <albert.mavrin@gmail.com>
2024-10-26 23:43:51 +0300

    Рабочая версия без диодов

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 #include "DHT.h"
 #include "DHT_U.h"
 #include <GyverOLED.h>
-#include <microDS18B20.h>
 
 #define DHTTYPE DHT11 // DHT 11
 // #define DHTTYPE    DHT22     // DHT 22 (AM2302)
@@ -16,12 +15,8 @@ DHT_Unified dht(DHTPIN, DHTTYPE);
 #define TEMPERATURE_PRECISION 12 // точность измерений (9 ... 12)
 
 OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature flo_sensor(&oneWire);
+DallasTemperature flo_sensors(&oneWire);
 DeviceAddress Thermometer;
-
-// OneWire ds(ONE_WIRE_BUS);
-
-// MicroDS18B20<ONE_WIRE_BUS> flo_sensor;
 
 #define MOTION_PIN GPIO_NUM_12
 #define MOTION_CALM_DOWN 2000
@@ -44,7 +39,12 @@ void showInfo(float TA, float HA, float TF)
 
     //    oled.setCursorXY(64, 0); // курсор в 0,0
     oled.print("Пол: ");
-    oled.print(TF, 1);
+    oled.println(TF, 1);
+
+    oled.print("ADDR: ");
+    for(int i = 0; i < 8; i++){
+        oled.print(Thermometer[i], HEX);
+    }
 }
 
 void IRAM_ATTR detectsMovement()
@@ -89,8 +89,8 @@ void taskMovement(void *pv)
 
 void setup()
 {
-    // pinMode(ONE_WIRE_BUS, INPUT);
-
+    pinMode(ONE_WIRE_BUS, INPUT_PULLUP);
+    pinMode(LED_BUILTIN, OUTPUT);
 
     Serial.begin(115200);
 
@@ -100,16 +100,15 @@ void setup()
     dht.temperature().getSensor(&dht_sensor);
     dht.humidity().getSensor(&dht_sensor);
 
-    flo_sensor.begin();                                           // инициализация DS18B20
-    flo_sensor.getAddress(Thermometer, 0);                        // адрес DS18B20 (поиск по индексу)
-    flo_sensor.setResolution(Thermometer, TEMPERATURE_PRECISION); // установка точности измерения 9...12 разрядов
+    flo_sensors.begin();                                           // инициализация DS18B20
+    bool has_address = flo_sensors.getAddress(Thermometer, 0);                        // адрес DS18B20 (поиск по индексу)
+    digitalWrite(LED_BUILTIN, has_address);
+    // flo_sensor.setResolution(Thermometer, TEMPERATURE_PRECISION); // установка точности измерения 9...12 разрядов
 
     // pinMode(MOTION_PIN, INPUT_PULLUP);
 
     // attachInterrupt(digitalPinToInterrupt(MOTION_PIN), detectsMovement, CHANGE);
 
-    pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, LOW);
 
     oled.init(GPIO_NUM_12, GPIO_NUM_11); // инициализация
     oled.clear();                        // очистка
@@ -144,34 +143,9 @@ void loop()
         hin = event.relative_humidity;
     }
 
-    flo_sensor.requestTemperatures();        // считывание значение температуры
-    tfl = flo_sensor.getTempC(Thermometer); // температура в градусах Цельсия
-
-    // if (!ds.search(addr))
-    // {
-    //     Serial.println("No more addresses.");
-    //     Serial.println();
-    //     ds.reset_search();
-    // }
-    // else
-    // {
-    //     Serial.print("ROM =");
-    //     for (int i = 0; i < 8; i++)
-    //     {
-    //         Serial.write(' ');
-    //         Serial.print(addr[i], HEX);
-    //     }
-    // }
-
-    // flo_sensor.requestTemp();
-    // vTaskDelay(1000);
-    // if (flo_sensor.readTemp()) {
-    //     tfl = flo_sensor.getTemp();
-    //     Serial.println(tfl);
-    // }
-    // else {
-    //     Serial.println("Not ready");
-    // }
+    flo_sensors.requestTemperatures();       // считывание значение температуры
+    // tfl = flo_sensors.getTempCByIndex(0);
+    tfl = flo_sensors.getTempC(Thermometer); // температура в градусах Цельсия
 
     showInfo(tin, hin, tfl);
 
